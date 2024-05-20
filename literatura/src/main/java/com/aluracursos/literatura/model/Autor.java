ad1813bb8b5b2ecaebf9fef9af7b39a354389549
“elif-cotton” <elif.watercolors@gmail.com>
2024-05-20 18:26:26 -0400

    challenge spring libros

---
+++ literatura/src/main/java/com/aluracursos/literatura/model/Autor.java
@@ -0,0 +1,72 @@
+package com.aluracursos.literatura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String fechaDeNacimiento;
+    private String fechaDeFallecimiento;
+
+
+    public Autor() {
+    }
+
+    public Autor(DatosAutor datosAutor) {
+        this.nombre = datosAutor.nombre();
+        this.fechaDeNacimiento = datosAutor.fechaDeNacimiento();
+        this.fechaDeFallecimiento = datosAutor.fechaDeFallecimiento();
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre='" + nombre + '\'' +
+                ", fecha de nacimiento =" + fechaDeNacimiento +
+                ", fecha de fallecimiento='" + fechaDeFallecimiento + '\'';
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public String getFechaDeFallecimiento() {
+        return fechaDeFallecimiento;
+    }
+
+    public void setFechaDeFallecimiento(String fechaDeFallecimiento) {
+        this.fechaDeFallecimiento = fechaDeFallecimiento;
+    }
+
+
+}
---
+++ literatura/src/main/java/com/aluracursos/literatura/model/Libro.java
@@ -0,0 +1,90 @@
+package com.aluracursos.literatura.model;
+
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "libros")
+public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)  //único
+    private String titulo;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "libros_autores",
+            joinColumns = @JoinColumn(name = "libro_id"),
+            inverseJoinColumns = @JoinColumn(name = "autor_id"))
+    private List<Autor> autores;
+    private List<String> idiomas;
+    private Double numeroDeDescargas;
+
+    public Libro(){}
+
+    public Libro(String titulo,  List<Autor> autores, List<String> idiomas, Double numeroDeDescargas) {
+        this.titulo = titulo;
+        this.autores = autores;
+        this.idiomas = idiomas;
+        this.numeroDeDescargas = numeroDeDescargas;
+    }
+
+    public Libro(DatosLibro datosLibro){
+        this.titulo = datosLibro.titulo();
+        this.idiomas = datosLibro.idiomas();
+        this.numeroDeDescargas = datosLibro.numeroDeDescargas();
+    }
+
+    @Override
+    public String toString() {
+        return "Título=" + titulo + "\n" +
+                "Autor =" + autores + "\n" +
+                "Idiomas=" + idiomas + "\n" +
+                "Número de descargas=" + numeroDeDescargas + "\n";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public  List<Autor> getAutor() {
+        return autores;
+    }
+
+    public void setAutor( List<Autor> autores) {
+        //autor.forEach(a -> a.setLibro(this));
+        this.autores = autores;
+    }
+
+    public List<String> getIdiomas() {
+        return idiomas;
+    }
+
+    public void setIdiomas(List<String> idiomas) {
+        this.idiomas = idiomas;
+    }
+
+    public Double getNumeroDeDescargas() {
+        return numeroDeDescargas;
+    }
+
+    public void setNumeroDeDescargas(Double numeroDeDescargas) {
+        this.numeroDeDescargas = numeroDeDescargas;
+    }
+}
